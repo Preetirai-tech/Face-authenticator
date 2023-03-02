b21c24d27348f16911aab78baceea12cf6de7996
Preeti Rai <preetirai1812@yahoo.com>
2023-03-02 18:06:40 +0530

    final commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+# Declaring variables for setup functions
+PROJECT_NAME = "Face Authenticator"
+DESRCIPTION = "This is a Face Authenticator Project"
+
+
+setup(
+    name=PROJECT_NAME,
+    description=DESRCIPTION,
+    packages=find_packages(),
+)
